72f27f38a8d869be792a362cef10c86520ab77bc
Dmitry Khrunin <thesunshineofficial@gmail.com>
2025-10-22 09:56:48 +0300

    native clickhouse connection

--- db/click.go
+++ db/click.go
@@ -4,10 +4,10 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/uptrace/opentelemetry-go-extra/otelsqlx"
-
-	_ "github.com/ClickHouse/clickhouse-go/v2" // clickhouse driver
+	"github.com/ClickHouse/clickhouse-go/v2"
+	"github.com/ClickHouse/clickhouse-go/v2/lib/driver"
 	"github.com/jmoiron/sqlx"
+	"github.com/uptrace/opentelemetry-go-extra/otelsqlx"
 )
 
 const clickhouseDriver = "clickhouse"
@@ -31,8 +31,33 @@ func NewClickhouse(ctx context.Context, connectionString string, options ...Opti
 	}
 
 	if err = db.PingContext(ctx); err != nil {
-		return nil, err
+		return nil, fmt.Errorf("can't ping connection: %w", err)
 	}
 
 	return db, nil
+}
+
+func NewNativeClickhouse(ctx context.Context, clickhouseOptions *clickhouse.Options) (driver.Conn, error) {
+	conn, err := clickhouse.Open(clickhouseOptions)
+	if err != nil {
+		return nil, fmt.Errorf("can't open connection: %w", err)
+	}
+
+	if err = conn.Ping(ctx); err != nil {
+		return nil, fmt.Errorf("can't ping connection: %w", err)
+	}
+
+	return conn, nil
+}
+
+func NewClickhouseOptions(host string, port int, database, username, password string) *clickhouse.Options {
+	return &clickhouse.Options{
+		Protocol: clickhouse.Native,
+		Addr:     []string{fmt.Sprintf("%s:%d", host, port)},
+		Auth: clickhouse.Auth{
+			Database: database,
+			Username: username,
+			Password: password,
+		},
+	}
 }
