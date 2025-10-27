6e3fe98d07cfabb380daaf5ef60d40f29c1cbbff
Dmitry Khrunin <thesunshineofficial@gmail.com>
2025-10-27 21:49:31 +0300

    db named get

--- db/tx.go
+++ db/tx.go
@@ -1,6 +1,7 @@
 package db
 
 import (
+	"context"
 	"errors"
 	"fmt"
 
@@ -11,6 +12,33 @@ func NamedGet(tx *sqlx.Tx, dest any, query string, arg any) error {
 	rows, err := tx.NamedQuery(query, arg)
 	if err != nil {
 		return fmt.Errorf("tx.NamedQuery: %w", err)
+	}
+	defer func() {
+		err = errors.Join(err, fmt.Errorf("close rows: %w", rows.Close()))
+	}()
+
+	if !rows.Next() {
+		err = errors.New("no rows returned")
+		return err
+	}
+
+	if err = rows.StructScan(dest); err != nil {
+		err = fmt.Errorf("rows.StructScan: %w", err)
+		return err
+	}
+
+	if err = rows.Err(); err != nil {
+		err = fmt.Errorf("rows.Err: %w", err)
+		return err
+	}
+
+	return nil
+}
+
+func NamedGetWithDB(ctx context.Context, db *sqlx.DB, dest any, query string, arg any) error {
+	rows, err := db.NamedQueryContext(ctx, query, arg)
+	if err != nil {
+		return fmt.Errorf("db.NamedQueryContext: %w", err)
 	}
 	defer func() {
 		err = errors.Join(err, fmt.Errorf("close rows: %w", rows.Close()))
